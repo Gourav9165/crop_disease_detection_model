995eebb9b63a65f326a1e4da35b9dad12422c3df
Gourav Sen <sen9165812790@gmail.com>
2024-08-18 13:04:49 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -3,13 +3,13 @@
 echo "BUILD START"
 
 # Install pip if not installed
-python3.9 -m ensurepip --upgrade
+python3.10 -m ensurepip --upgrade
 
 # Install the dependencies
-python3.9 -m pip install --upgrade pip
-python3.9 -m pip install -r requirements.txt
+python3.10 -m pip install --upgrade pip
+python3.10 -m pip install -r requirements.txt
 
 # Collect static files
-python3.9 manage.py collectstatic --noinput --clear
+python3.10 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
