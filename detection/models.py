e07f5c34341ef6fe161002065b485aa338220972
Gourav Sen <sen9165812790@gmail.com>
2024-08-30 22:43:28 +0530

    Delete detection directory

--- detection/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class UploadedImage(models.Model):
-    image = models.ImageField(upload_to='images/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
--- detection/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('upload_image/', views.upload_image, name='upload_image'),
-    path('enter_url/', views.enter_url, name='enter_url'),
-]
